dc72e929a8b15d36ca1e7e4c537328725306f22c
Nello-Moreira <g.moreira1@outlook.com>
2021-12-02 15:01:43 -0300

    implemented: insert recomendation (recomendation repository)

---
+++ src/repositories/recomendations.js
@@ -0,0 +1,29 @@
+import dbConnection from './connection.js';
+
+async function insertRecomendation({ name, youtubeLink, score }) {
+	const recomendation = await dbConnection.query(
+		'INSERT INTO songs (name, youtube_link, score) VALUES ($1, $2, $2) RETURNING id;',
+		[name, youtubeLink, score]
+	);
+
+	return recomendation.rows[0].id;
+}
+
+async function insertRecomendationGenres({ recomendationId, genresIds }) {
+	const query = [];
+
+	genresIds.forEach((genreId) => {
+		query.push(`(${recomendationId}, ${genreId})`);
+	});
+
+	await dbConnection.query(
+		`INSERT INTO songs_genres
+            (song_id, genre_id)
+        VALUES
+            ${query.join(',')};`
+	);
+
+	return true;
+}
+
+export default { insertRecomendation, insertRecomendationGenres };
