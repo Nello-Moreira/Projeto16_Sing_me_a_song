6fd1a0ada4b64bc548909ab7c51abeab4b30db64
Nello-Moreira <g.moreira1@outlook.com>
2021-12-02 09:57:05 -0300

    created database tables script

--- database_scripts/scripts/create-tables.sql
+++ database_scripts/scripts/create-tables.sql
@@ -1,4 +1,40 @@
-CREATE TABLE "examples" (
-  "id" SERIAL PRIMARY KEY,
-  "user_id" TEXT NOT NULL
+CREATE TABLE "songs" (
+	"id" serial NOT NULL,
+	"name" TEXT NOT NULL,
+	"youtube_link" TEXT NOT NULL,
+	"score" integer NOT NULL,
+	CONSTRAINT "songs_pk" PRIMARY KEY ("id")
+) WITH (
+  OIDS=FALSE
 );
+
+
+
+CREATE TABLE "songs_genres" (
+	"id" serial NOT NULL,
+	"song_id" integer NOT NULL,
+	"genre_id" integer NOT NULL,
+	CONSTRAINT "songs_genres_pk" PRIMARY KEY ("id")
+) WITH (
+  OIDS=FALSE
+);
+
+
+
+CREATE TABLE "genres" (
+	"id" serial NOT NULL,
+	"name" TEXT NOT NULL,
+	CONSTRAINT "genres_pk" PRIMARY KEY ("id")
+) WITH (
+  OIDS=FALSE
+);
+
+
+
+
+ALTER TABLE "songs_genres" ADD CONSTRAINT "songs_genres_fk0" FOREIGN KEY ("song_id") REFERENCES "songs"("id");
+ALTER TABLE "songs_genres" ADD CONSTRAINT "songs_genres_fk1" FOREIGN KEY ("genre_id") REFERENCES "genres"("id");
+
+
+
+
