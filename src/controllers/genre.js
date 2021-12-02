95d7745e552cac4f0b12792fa04cbf0413a4e042
Nello-Moreira <g.moreira1@outlook.com>
2021-12-02 14:43:08 -0300

    implemented: status codes object

--- src/controllers/genre.js
+++ src/controllers/genre.js
@@ -2,15 +2,16 @@ import genresService from '../services/genre.js';
 import NoContent from '../errors/NoContent.js';
 import Conflict from '../errors/Conflict.js';
 import { isInvalidGenreName } from '../validation/genre.js';
+import statusCodes from '../helpers/statusCodes.js';
 
 async function getAllGenres(request, response, next) {
 	try {
 		const genres = await genresService.searchAllGenres();
 
-		return response.status(200).send(genres);
+		return response.status(statusCodes.ok).send(genres);
 	} catch (error) {
 		if (error instanceof NoContent) {
-			return response.sendStatus(204);
+			return response.sendStatus(statusCodes.noContent);
 		}
 
 		return next(error);
@@ -23,16 +24,16 @@ async function postGenre(request, response, next) {
 	const genreNameError = isInvalidGenreName({ name });
 
 	if (genreNameError) {
-		return response.status(400).send(genreNameError.message);
+		return response.status(statusCodes.badRequest).send(genreNameError.message);
 	}
 
 	try {
 		const genreId = await genresService.insertGenre({ genreName: name });
 
-		return response.status(201).send({ id: genreId });
+		return response.status(statusCodes.created).send({ id: genreId });
 	} catch (error) {
 		if (error instanceof Conflict) {
-			return response.status(409).send(error.message);
+			return response.status(statusCodes.conflict).send(error.message);
 		}
 
 		return next(error);
---
+++ src/helpers/statusCodes.js
@@ -0,0 +1,10 @@
+const statusCodes = {
+	ok: 200,
+	created: 201,
+	noContent: 204,
+	badRequest: 400,
+	notFound: 404,
+	conflict: 409,
+};
+
+export default statusCodes;
