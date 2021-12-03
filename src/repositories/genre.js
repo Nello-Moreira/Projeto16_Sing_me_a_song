d4d7ade2881675267116cc75ac3b4a735676b8fe
Nello-Moreira <g.moreira1@outlook.com>
2021-12-03 19:02:08 -0300

    implemented: get genre with recomendations query function

--- src/repositories/genre.js
+++ src/repositories/genre.js
@@ -15,4 +15,53 @@ async function insertGenre({ genreName }) {
 	return genre.rows[0].id;
 }
 
-export default { searchAllGenres, insertGenre };
+async function searchGenreWithRecomendations({ id }) {
+	const genreQueryResult = await dbConnection.query(
+		`
+		SELECT * FROM genres WHERE id = $1;
+		`,
+		[id]
+	);
+
+	const genre = genreQueryResult.rows[0];
+
+	const recomendationsQueryResult = await dbConnection.query(
+		`
+	SELECT songs.*,
+		(
+			SELECT array_agg('{"id": ' || genres.id || ', "name": "' || genres.name || '"}') as genres
+			FROM genres
+			JOIN songs_genres
+				ON genres.id = songs_genres.genre_id
+			WHERE songs_genres.song_id = songs.id
+			GROUP BY songs_genres.song_id
+		)
+	FROM songs
+	JOIN songs_genres
+		ON songs_genres.song_id = songs.id
+	WHERE songs_genres.genre_id = $1;
+	`,
+		[id]
+	);
+
+	const formattedRecomendations = recomendationsQueryResult.rows.map(
+		(song) => ({
+			...song,
+			genres: song.genres.map((genreString) => JSON.parse(genreString)),
+		})
+	);
+
+	const scores = formattedRecomendations.map(
+		(recomendation) => recomendation.score
+	);
+
+	const genreScore = scores.reduce(
+		(previousValue, currentValue) => previousValue + currentValue
+	);
+
+	genre.recomendations = formattedRecomendations;
+	genre.score = genreScore;
+
+	return genre;
+}
+export default { searchAllGenres, searchGenreWithRecomendations, insertGenre };
